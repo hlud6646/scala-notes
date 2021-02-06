3f54e6bad98bfc97f41d2a9c807afb5a615cc9fb
hlud6646 <hugo.ludemann@gmail.com>
2021-02-06 19:29:31 +1100

    finished arithemtic

--- src/main/scala/ninetyNine/Arithmetic.scala
+++ src/main/scala/ninetyNine/Arithmetic.scala
@@ -30,6 +30,34 @@ object Arithmetic extends App {
       }
     help(n).groupBy(identity).mapValues(_.size)
   }
-  println(primeFactorMultiplicity(315))
+  
+  // 37.
+  def totient(n: Int) = primeFactorMultiplicity(n)
+                        .map(kv => math.pow(kv._1,  (kv._2 - 1)).toInt * (kv._1 - 1))
+                        .product
+
+  // 39.
+  def listPrimes(a: Int, b: Int) = (2 to b) filter isPrime
+
+  // 40. Goldbach's conjecture: Every odd even number greater than 2 can 
+  // be writen as the sum of two primes.
+  def goldbach(n: Int) = {
+    val p = listPrimes(3, n)
+    (for {
+      a <- p
+      b <- p if a + b == n
+    } yield (a, b)).head
+  }
+
+  // 41. Print the Goldbach pair for all even numbers in a given range.
+  // No guards or anything on this; dies if a == 2 or either is odd. 
+  def printGoldbach(a: Int, b: Int) =  (a to b by 2).map(goldbach).foreach{
+      case (x, y) => println(s"${x + y} = $x + $y")
+    }
+  def printGoldbachLimited(a: Int, b: Int, m: Int) =
+    (a to b by 2).map(goldbach).map(t => List(t._1, t._2)).filter(_.min >= m) foreach {
+      t => println(s"${t(0) + t(1)} = $t(0) + $t(1)")
+    }
+
 }
 
